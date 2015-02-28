2495d3fe4d23040b4c9ad4c4fd05b372be233444
waiwaibaka <virtue.sign@gmail.com>
2015-02-27 22:31:52 -0800

    Test Branch commit

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
     app.setOrganizationName("PW & Friend");
     app.setOrganizationDomain("waiwaibaka.com");
-    app.setApplicationName("myQtTemplate");
+    app.setApplicationName("InLov");
 
     theApp.start();
 
